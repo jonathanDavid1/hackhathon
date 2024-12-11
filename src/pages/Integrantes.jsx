8cdb77575057fd731badf546b0b7e0b6a7bd628e
Jonathan David <76568639+jonathanDavid1@users.noreply.github.com>
2024-12-11 10:10:18 -0500

    Update Integrantes.jsx

--- src/pages/Integrantes.jsx
+++ src/pages/Integrantes.jsx
@@ -32,9 +32,9 @@ function Integrantes() {
             </div>
             <div className="p-4 md:w-1/3">
             <div className="h-full bg-gray-100 p-8 rounded">
-            <img src="/images/giselle.jpg" alt="Jonathan Hernández" className="w-24 h-24 rounded-full mx-auto" />
-              <h2 className="text-xl font-medium title-font text-gray-900">Gisselle Támara</h2>
-              <p className="text-gray-500">Ingeniera en Sistemas</p>
+            <img src="/images/yesid.jpg" alt="Jonathan Hernández" className="w-24 h-24 rounded-full mx-auto" />
+              <h2 className="text-xl font-medium title-font text-gray-900">Yesid Vera</h2>
+              <p className="text-gray-500">Abogado</p>
             </div>
           </div>
           </div>
